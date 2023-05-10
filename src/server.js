215303468981a529daf562f690faafde85b921be
anhhung294 <hoanganhhung29042004@gmail.com>
2023-05-10 20:54:55 +0700

    error handling

--- src/server.js
+++ src/server.js
@@ -180,7 +180,7 @@ app.post(
         } catch (err) {
             err.httpStatusCode = 400;
             console.log(err);
-            return next(new Error("Something went wrong"));
+            return res.status(500).send("Some thing went wrong");
         }
     }
 );
