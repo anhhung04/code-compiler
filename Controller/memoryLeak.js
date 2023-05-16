3efb9db9d7a2d8ca460d4bdfae14002b403327b4
anhhung294 <hoanganhhung29042004@gmail.com>
2023-05-16 21:15:08 +0700

    f

--- Controller/memoryLeak.js
+++ Controller/memoryLeak.js
@@ -35,7 +35,7 @@ async function sendMemoryLeakFiles(req, res, next) {
             await writeFile(`./${std_id}/events.txt`, event);
             await writeFile(`./${std_id}/knights.txt`, knight);
             const { stderr: outErr, stdout: outOut } = await exec(
-                `valgrind --leak-check=ful ${path.join(
+                `valgrind --leak-check=full ${path.join(
                     __dirname,
                     `../${std_id}/main`
                 )} ./${std_id}/knights.txt ./${std_id}/events.txt`
